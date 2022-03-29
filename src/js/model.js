f1eb9941a559af633a21a77dd1678883384a6695
barhome <53843444+Barhome@users.noreply.github.com>
2022-03-29 19:14:58 +0200

    feat:refactoring the code into MVC architectural structure

---
+++ src/js/model.js
@@ -0,0 +1,33 @@
+import { async } from "regenerator-runtime";
+console.log("test");
+
+export const state = {
+  recipe: {},
+};
+
+// function to change state.recipe and the controller will grab it.
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message}`);
+    const { recipe } = data.data;
+
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
